502fe00eb551a5831cdc3d908a449bcfec00d0cf
Joshua Stein <josh.stein107@gmail.com>
2013-03-26 03:38:25 -0400

    commented out s3.rb

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,3 +1,4 @@
+=begin
 if Rails.env == "production" 
    S3_CREDENTIALS = { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :bucket => "production-routines"} 
  else 
@@ -5,3 +6,4 @@ if Rails.env == "production"
 end
 service = S3::Service.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
                           :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
+=end
